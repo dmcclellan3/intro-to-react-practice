46d9ce3d64a46904df4c9b087e7cfa22b91801e0
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-22 16:44:09 -0400

    save

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const Title = () => {
   return (
-    <h1>
+    <h1 className="text-center">
       Hello World!
     </h1>
   )
@@ -15,13 +15,12 @@ const Title = () => {
 function App() {
   const [counter, setCounter] = useState(0);
   const incrementCounter = () => setCounter(counter + 1);
-  let decrementCounter = () => setCounter(counter - 1);
+  const decrementCounter = () => setCounter(counter - 1);
 
   return (
     <div 
-      className="bg-dark h-100 p-5"
-      style={{ color: 'white'}}
-    >
+      className="bg-dark h-100 p-5 border border-5"
+      style={{ color: 'white'}}>
       <Link className="text-light" to='./About'>About</Link>
       <br></br>
       <Link className="text-light" to='./Contacts'>Contacts</Link>
@@ -30,9 +29,9 @@ function App() {
         onClick={incrementCounter}
         >
         <button type="button" className="btn btn-primary">Increment</button>
-        <br></br>
-        <br></br>
       </div>
+        <br></br>
+        <br></br>
         <div className="decrement-counter-button" onClick={decrementCounter}>
         <button type="button" className="btn btn-primary">Decrement</button>
          
